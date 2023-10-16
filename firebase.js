7ec0daf99fc25c5e4813d7a69ba9c7782ae52a05
rohan9024 <rohanwandre24@gmail.com>
2023-10-16 23:54:01 +0530

    Fixed the env issue

--- firebase.js
+++ firebase.js
@@ -2,15 +2,17 @@ import { getApp, getApps, initializeApp } from "firebase/app"
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import dotenv from "dotenv";
+dotenv.config();
 
 const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.measurementId
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 
